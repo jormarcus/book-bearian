fde6f39f13f77818c74face37a8b67fadef40d40
Jordan Marcus <jormarcus92@gmail.com>
2020-05-03 14:39:39 -0400

    google api services

---
+++ src/services/GoogleServices.ts
@@ -0,0 +1,21 @@
+import BaseService from './BaseService';
+import { config } from '../config/keys';
+
+class GoogleServices {
+  public static getBookByAuthorOrTitle = (
+    title: string = '',
+    author: string = ''
+  ) => {
+    return BaseService.GET(
+      `https://www.googleapis.com/books/v1/volumes?q={title}+inauthor:{author}&key=${config.key}`
+    );
+  };
+
+  public static getBookShelvesByUserId(userId) {
+    return BaseService.GET(
+      `https://www.googleapis.com/books/v1/users/${config.userId}/bookshelves&key=${config.key}`
+    );
+  }
+}
+
+export default GoogleServices;
